05f124f63e2a21f84cc3c51fcbaf6ff590d816c5
Shafiera <35319032+Shafiera@users.noreply.github.com>
2020-07-21 22:33:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+/*saat tombol di klik ada 3 tahapan:
+1. mendeteksi aksi klik tombol
+2. mengambil angka tombol yang di klik
+3. jalankan function untuk menampilkan angka di layar display*/
+
+/*definisikan function utk memperbarui layar tampilan*/
+const calculatorScreen = document.querySelector('.calculator-screen')
+const updateScreen = (number) => {
+  calculatorScreen.value = number
+}
+
+/*code mendeteksi adanya klik sebuah tombol: men-setting "click event" dgn event listener*/
 const numbers = document.querySelectorAll(".number")
-console.log(numbers)
+numbers.forEach((number) =>{
+  number.addEventListener("click", (event)=>{
+    updateScreen(event.target.value)
+  })  
+})
+
