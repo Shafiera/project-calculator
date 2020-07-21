7192bf171e0ac862d25128cbc76228b028f93e87
Shafiera <35319032+Shafiera@users.noreply.github.com>
2020-07-21 23:29:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,15 @@
 2. mengambil angka tombol yang di klik
 3. jalankan function untuk menampilkan angka di layar display*/
 
+/*code mendeteksi adanya klik sebuah tombol: men-setting "click event" dgn event listener*/
+const numbers = document.querySelectorAll(".number")
+numbers.forEach((number) =>{
+  number.addEventListener("click", (event)=>{
+    console.log(event.target.value)
+  })  
+})
+
+
 /*definisikan function utk memperbarui layar tampilan*/
 const calculatorScreen = document.querySelector('.calculator-screen')
 const updateScreen = (number) => {
